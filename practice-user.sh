128ab2c180252cf4fb9240b74a13f3d01fb7ce8e
srinivas gonepudi <srigonepudi@gmail.com>
2023-12-26 19:12:09 +0530

    added new instnace

--- practice-user.sh
+++ practice-user.sh
@@ -9,26 +9,21 @@ if [ ${who} == "root" ]; then
 else 
     echo "your are not root user please switch to root"
 
-fi    
+fi
+
+validation () {
+
+if [ $1 -ne 0 ]; then
+
+    echo "$2 .... Failed"
+else
+     echo "$2 ... Success"
+fi        
+
+}
 read -p "Enter User name :" username
 echo
-while true; do
 
-    if [ -z $username ]; then
-
-        read -p "Username should not be empty: " username
-        echo
-    else
-     echo "username is:" $username
-
-    fi
-
-    if [ ${user_validation} -eq 0 ]; then
-
-        echo "user existed Please enter another user name"
-        exit 1
-    else
-        echo " $username is new user please proceed"
-    fi        
-    
-done         
+getent passwd | greap $username
+validation $? "user creation"
+         
