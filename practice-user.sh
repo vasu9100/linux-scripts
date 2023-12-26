23a098d17a3e00a6d797655ec80c2fd6059a8326
srinivas gonepudi <srigonepudi@gmail.com>
2023-12-26 19:25:35 +0530

    added new instnace

--- practice-user.sh
+++ practice-user.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+NC='\033[0m'
+
 who=$(whoami)
 user_validation=$(getent passwd | grep $username)
 
@@ -15,14 +21,14 @@ validation () {
 
 if [ $1 -ne 0 ]; then
 
-    echo "$2 .... is not existed Please proceed"
+    echo -e "$2 .... ${GREEN}is not existed Please proceed"
 else
-     echo "$2 ... existed"
+     echo -e "$2 ... ${RED}existed"
      exit 1
 fi        
 
 }
-echo "Please verify frist user is existed or not in system "
+echo -e "${YELLOW}Please verify frist user is existed or not in system "
 
 read -p "Enter new User name :" username
 echo
@@ -31,3 +37,4 @@ getent passwd ${username}
 validation $? "user"
 
 useradd ${username}
+echo "${username} added successfully"
