457e2ca24e26b329b427f6af6213c42e96d21bc4
srinivas gonepudi <srigonepudi@gmail.com>
2023-12-26 18:48:58 +0530

    added new instnace

--- practice-user.sh
+++ practice-user.sh
@@ -1,58 +1,11 @@
 #!/bin/bash
-ID=$(id -u)
+who=$(whoami)
 
-if [ $ID -ne 0 ]; then
+if [ ${who} == "root" ]; then
 
-    echo "You must switch into root account"
-    exit 1
-fi   
+    echo "your are a root user"
 
-read -p "Enter Username: " username
-echo
+else 
+    echo "your are not root user please switch to root"
 
-while true; do
-
-    read -p "Please Enter Username: " username
-
-    if [ -z "$username" ]; then
-        echo "Username cannot be empty. Please try again."
-    else
-    
-    id $username
-
-    if [ $? -ne 0 ]; then
-        echo "this user new user"
-    else
-        echo "user existed"
-        exit 1
-    fi
-  fi         
-    
-done
-
-read -p "Enter password: " password
-echo
-
-while [ -z "$password" ]; do
-
-    read -s -p "Please Enter Password: " password
-    
-done
-read -P "Enter User permissions: " userpermissions
-echo
-read -P "Enter User permissions: " grouppermissions
-echo
-read -P "Enter User permissions: " otherpermissions
-echo
-
-while [ -z "$username" ]; do
-
-    read -p "Please Enter Username: " username
-    
-done    
-
-while [ -z "$password" ]; do
-
-    read -s -p "Please Enter Password: " password
-    
-done 
+fi    
